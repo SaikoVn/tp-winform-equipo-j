dacf1d0bd825d16de0aa160dffa729d2625c77e6
Fernando Quiroga <quirogabyuv@gmail.com>
2026-09-07 16:44:03 -0300

    Creé la solución 'TPWinForm_equipo-J' y creé sus respectivos proyectos de .NET Framework (en total 3), tal como hizo Maxi en su ejemplo de Repo 'https://github.com/msarfernandez/ejemplos-ado-net/tree/main'. dominio: contiene solamente las definiciones de las entidades de cada tabla (Marca, Categoria, Imagenes y Articulo) con sus atributos y propiedades. negocio: se encarga de la logica de procesamiento de datos y conexión con la base de datos. winform-app: el formulario donde se captura las acciones del usuario, se muestra (lo visual) y delega el trabajo a negocio. Asimismo, entablé las relaciones entre proyectos: negocio referencia a dominio, y winform-app referencia tanto a dominio como a negocio. dominio es totalmente autonomo de los demas: en cambio, los otros necesitan de él. una vez hecho esto, me encargué de poner los atributos y propiedades de las 4 clases de dominio, y hacer un comienzo de la parte negocio: los atributos privados para la conexion, consulta y la lectura, y la propiedad publica para este ultimo. Nos quedaría terminar la lógica de la parte dominio, y ya con eso podriamos avanzar a la parte de formulario que sería lo más fácil (obviamente para probar la logica de negocio podriamos construir algo en form).

---
+++ TPWinForm_equipo-J/Form1.cs
@@ -0,0 +1,10 @@
+namespace TPWinForm_equipo_J
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector
+         {
+            get { return lector; }
+        }
+
+        public AccesoDatos() { 
+        // INICIALIZAR LA CONEXION Y EL COMANDO
+        // ---
+        // ---
+        } 
+    }
+}
